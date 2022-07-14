6d28ac0a5ba46795cdace1612890dd1f29f8bcab
AIDRAGONcyl <2913167220@qq.com>
2022-07-15 00:38:16 +0800

    7.14-格式改

--- week01/hello.cpp
+++ week01/hello.cpp
@@ -1,7 +1,26 @@
+/**
+ * @file hello.cpp
+ * @brief 
+ * @author AIDRAGONcyl (2913167220@qq.com)
+ * @date 2022-07-14
+ * 
+ * @copyright Copyright (C) 2022, HITCRT_VISION, all rights reserved.
+ * 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date       <th>Author  <th>Description
+ * <tr><td>2022-07-14 <td>AIDRAGONcyl  <td>
+ * </table>
+ */
 #include<iostream>
-
-int main() {
-    std::cout << "Hello World!" << std::endl;
+/**
+ * @brief 
+ * @return int 
+ * @author AIDRAGONcyl (2913167220@qq.com)
+ */
+int main()
+{
+    std::cout << "Hello,world!" << std::endl;
     getchar();
     return 0;
 }
